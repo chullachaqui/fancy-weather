199285848722129d6c9c725dd4cf8deeb6d25194
chullachaqui <80277029+chullachaqui@users.noreply.github.com>
2021-08-08 20:25:42 +0200

    feat!: add functionalities and css styling
    add voice search, improve background generating,

--- index.js
+++ index.js
@@ -6,18 +6,45 @@ class WeatherApp {
         
         this.dashboard = new Dashboard();
         this.weather = new Weather();
-        this.map = new Map();
+        this.map = new weatherMap();
+        this.speech = new Speech();
         this.weather.language = this.language;
         this.map.language = this.language;
         
         this.searchListeners();
         this.unitChangeListeners();
         this.languageListeners();
+        this.backgroundListeners();
+        this.speech.voiceSearchListener();
     }
 
     generateHTML() {
         this.dashboardElement = new HTMLelement('header', document.body, 'dashboard');
         this.mainElement = new HTMLelement('main', document.body, 'main');
+    }
+
+    getBackground() {
+        // fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=bd26977b2f064d0d84133023c5cac316&tags=weather,${this.weather.description.innerHTML}&tag_mode=all&accuracy=11&extras=url_h,url_l&geo_context=2&format=json&nojsoncallback=1`)
+        // // .then(response => console.log(response))
+        // .then(response => response.json())
+        // .then(object => {
+        //     console.log(object);
+        //     const randomIndex = Math.floor(Math.random() * 100);
+        //     console.log(randomIndex);
+
+        //     const randomPhoto = object.photos.photo[randomIndex];
+        //     const url = randomPhoto.url_l ? randomPhoto.url_l : randomPhoto.url_h
+        //     // if (!url) { url = `${}` }
+        //     // const url = `https://live.staticflickr.com/${randomPhoto.server}/${randomPhoto.id}_${randomPhoto.secret}_b.jpg`
+        //     document.body.setAttribute('style', `background-image: url(${url});`);
+        // })
+
+        fetch(`https://api.unsplash.com/photos/random?&client_id=L0JtEhlUZnLRnHtLs5d8-BZBPQNp-Pejvs5PKObs7eE&orientation=landscape&per_page=1&query=sky,weather`)
+        .then(response => response.json())
+        .then(data => {
+            const url = data.urls.regular;
+            document.body.setAttribute('style', `background-image: url(${url});`);
+        })
     }
 
     // weather corresponding to device's location:
@@ -35,13 +62,14 @@ class WeatherApp {
     }
 
     getLocationBasedWeather(location) {
-        // getBackground();
         // tutaj po searchu podajesz co wpisano w searchBar, ponizszy kod go przemienia na lat i long, które później podajesz do getweather
         fetch(`https://api.opencagedata.com/geocode/v1/json?q=${location}&key=d1e4f3c55f444cfa82590e0c3d733a98&limit=1&language=${this.language}`)
         .then((response) => response.json())
         .then((data) => {
             this.latitude = data.results[0].geometry.lat;
             this.longitude = data.results[0].geometry.lng;
+            this.getBackground();
+
                         
             this.map.centerPosition(this.latitude, this.longitude);
             this.map.displayCoordinates(this.latitude, this.longitude);
@@ -58,7 +86,7 @@ class WeatherApp {
             this.weather.getWeather(this.latitude, this.longitude);
         })
         .catch(() => {
-            return this.dashboard.language === 'en' ? alert('please retry with a valid location name') : alert('Spróbój ponownie wpisując poprawne dane') ;
+            return this.dashboard.language === 'en' ? alert('please retry with a valid location name') : alert('Spróbój ponownie wpisując poprawne dane');
         });
     }
 
@@ -80,11 +108,16 @@ class WeatherApp {
         this.dashboard.celsius.addEventListener('click', () => {
             this.weather.units = 'celsius';
             this.weather.getWeather(this.latitude, this.longitude);
+            this.dashboard.celsius.classList.add('active-button');
+            this.dashboard.fahrenheit.classList.remove('active-button');
+
         });
 
         this.dashboard.fahrenheit.addEventListener('click', () => {
             this.weather.units = 'fahrenheit';
-            this.weather.getWeather(this.latitude, this.longitude)
+            this.weather.getWeather(this.latitude, this.longitude);
+            this.dashboard.fahrenheit.classList.add('active-button');
+            this.dashboard.celsius.classList.remove('active-button');
         })
     }
     
@@ -121,6 +154,14 @@ class WeatherApp {
             }
         });
     }
+
+    backgroundListeners() {
+        const button = this.dashboard.changeBackground;
+        button.addEventListener('click', (event) => {
+            console.log(event);
+            this.getBackground();
+        })
+    }
 }
 
 class Dashboard {
@@ -129,38 +170,48 @@ class Dashboard {
 
         this.container = document.querySelector('.dashboard');
 
-        this.changeBackground = new HTMLelement('div', this.container, 'button');
+        const buttons = new HTMLelement('div', this.container, 'buttons');
+
+        this.changeBackground = new HTMLelement('div', buttons, 'button');
         this.changeBackground.button = new HTMLelement('span', this.changeBackground, 'material-icons-outlined');
         this.changeBackground.button.innerHTML = 'refresh';
 
-        this.changeLanguage = new HTMLelement('div', this.container, 'closed-list');
-        this.changeLanguage.chosen = new HTMLelement('div', this.changeLanguage, 'languageSelection');
-        this.changeLanguage.chosen.innerHTML = 'ENG';
+        const listContainer = new HTMLelement('div', buttons, 'listContainer');
+        this.changeLanguage = new HTMLelement('div', listContainer, 'closed-list');
+        this.changeLanguage.chosen = new HTMLelement('div', this.changeLanguage, 'languageSelected');
+        this.changeLanguage.chosen.innerHTML = localStorage.getItem('language') === 'en' ? 'ENG' : 'PL';
         this.changeLanguage.choice1 = new HTMLelement('div', this.changeLanguage, 'languageSelection');
         this.changeLanguage.choice1.innerHTML = 'ENG';
         this.changeLanguage.choice2 = new HTMLelement('div', this.changeLanguage, 'languageSelection');
         this.changeLanguage.choice2.innerHTML = 'PL';
- 
-        this.celsius = new HTMLelement('div', this.container, 'button');
+        
+        // const units = new HTMLelement('div', buttons, 'units');
+        this.celsius = new HTMLelement('div', buttons, 'units-c');
         this.celsius.innerHTML = '\xB0C';
-
-        this.fahrenheit = new HTMLelement('div', this.container, 'button');
+        this.celsius.classList.add('active-button');
+        
+        this.fahrenheit = new HTMLelement('div', buttons, 'units-f');
         this.fahrenheit.innerHTML = '\xB0F';
 
+        const searchArea = new HTMLelement('div', this.container, 'searchArea');
+
+        const voiceButton = new HTMLelement('div', searchArea, 'voiceButton');
+        this.voiceIcon = new HTMLelement('div', voiceButton, 'material-icons-outlined');
+        this.voiceIcon.innerHTML = 'mic';
+
         const placeholder = this.language === 'en' ? 'Search city or location' : 'Znajdź miasto lub miejsce';
-        this.searchBar = new HTMLelement('input', this.container, 'searchBar', { 'type': 'search', 'placeholder': placeholder });
+        this.searchBar = new HTMLelement('input', searchArea, 'searchBar', { 'type': 'search', 'placeholder': placeholder });
 
         const value = this.language === 'en' ? 'Search' : 'Szukaj';
-        this.searchButton = new HTMLelement('input', this.container, 'button', { 'type': 'button', 'value': value });
-                
+        this.searchButton = new HTMLelement('input', searchArea, 'searchButton', { 'type': 'button', 'value': value });
     }
+
+    
     
     translateDashboard() {
         const placeholder = this.language === 'en' ? 'Search city or location' : 'Znajdź miasto lub miejsce';
-        // this.searchBar = new HTMLelement('input', this.container, 'searchBar', { 'type': 'search', 'placeholder': placeholder });
-
         const value = this.language === 'en' ? 'Search' : 'Szukaj';
-        // this.searchButton = new HTMLelement('input', this.container, 'button', { 'type': 'button', 'value': value });
+
         this.searchBar.setAttribute('placeholder', placeholder);
         this.searchButton.setAttribute('value', value);
     }
@@ -174,14 +225,18 @@ class Weather {
         //today's weather:
         const weather = new HTMLelement('section', this.main, 'weather');
 
-        const today = new HTMLelement('div', weather, 'today');
+        const current = new HTMLelement('div', weather, 'location&time');
 
-        this.location = new HTMLelement('p', today, 'location');
-        this.time = new HTMLelement('p', today, 'time');
-        this.temperature = new HTMLelement('p', today, 'temperature-today');
-        this.icon = new HTMLelement('img', today, 'icon');
+        this.location = new HTMLelement('p', current, 'location');
+        this.time = new HTMLelement('p', current, 'time');
 
-        const overview = new HTMLelement('div', weather, 'today-overview');
+        const condition = new HTMLelement('div', current, 'condition');
+
+        this.temperature = new HTMLelement('p', condition, 'temperature-today');
+
+        const overview = new HTMLelement('div', condition, 'overview');
+
+        this.icon = new HTMLelement('img', overview, 'icon');
 
         this.description = new HTMLelement('p', overview, 'text');
         this.wind = new HTMLelement('p', overview, 'text');
@@ -190,20 +245,27 @@ class Weather {
 
         // following three days:
         const forecast = new HTMLelement('div', weather, 'forecast');
+
         this.day1 = new HTMLelement('div', forecast, 'forecast-day');
         this.day1.weekday = new HTMLelement('p', this.day1, 'forecast-weekday');
-        this.day1.icon = new HTMLelement('img', this.day1, 'forecast-icon');
-        this.day1.temperature = new HTMLelement('p', this.day1, 'forecast-temperature');
 
+        const condition1 = new HTMLelement('p', this.day1, 'forecast-condition');
+        this.day1.temperature = new HTMLelement('p', condition1, 'forecast-temperature');
+        this.day1.icon = new HTMLelement('img', condition1, 'forecast-icon');
+        
         this.day2 = new HTMLelement('div', forecast, 'forecast-day');
         this.day2.weekday = new HTMLelement('p', this.day2, 'forecast-weekday');
-        this.day2.icon = new HTMLelement('img', this.day2, 'forecast-icon');
-        this.day2.temperature = new HTMLelement('p', this.day2, 'forecast-temperature');
+
+        const condition2 = new HTMLelement('p', this.day2, 'forecast-condition');
+        this.day2.temperature = new HTMLelement('p', condition2, 'forecast-temperature');
+        this.day2.icon = new HTMLelement('img', condition2, 'forecast-icon');
 
         this.day3 = new HTMLelement('div', forecast, 'forecast-day');
         this.day3.weekday = new HTMLelement('p', this.day3, 'forecast-weekday');
-        this.day3.icon = new HTMLelement('img', this.day3, 'forecast-icon');
-        this.day3.temperature = new HTMLelement('p', this.day3, 'forecast-temperature');
+
+        const condition3 = new HTMLelement('p', this.day3, 'forecast-condition');
+        this.day3.temperature = new HTMLelement('p', condition3, 'forecast-temperature');
+        this.day3.icon = new HTMLelement('img', condition3, 'forecast-icon');
 
     }
 
@@ -211,7 +273,6 @@ class Weather {
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,alerts&cnt=3&appid=f9d19679058f35da8b74e2b7fb3b62a1&lang=${this.language}&units=metric`)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
             this.displayWeather(data);
         })
 
@@ -249,7 +310,7 @@ class Weather {
         this.feelsLike.innerHTML = this.language === 'en' ? `Feels like: ${this.feelsLike.value}` : `Temperatura odczuwalna: ${this.feelsLike.value}`;
         this.humidity.innerHTML = this.language === 'en' ? `Humidity: ${weatherData.current.humidity} %` : `Wilgotność: ${weatherData.current.humidity} %`;
 
-        // followig three days:
+        // following three days:
         for (let i = 1; i <= 3; i += 1) {
             let now = new Date();
             let today = now.getDate();            
@@ -263,19 +324,13 @@ class Weather {
     }
 }
 
-class Map {
+class weatherMap {
     constructor(/*latitude, longitude*/) {
         const main = document.querySelector('.main');
         this.container = new HTMLelement('section', main, 'map');
         
-        this.mapContainer = new HTMLelement('div', this.container, 'map', { id: 'map', style: 'width: 400px; height: 300px;'});
+        this.mapContainer = new HTMLelement('div', this.container, 'map', { id: 'map', style: 'width: 350px; height: 300px;'});
         mapboxgl.accessToken = 'pk.eyJ1IjoiY2h1bGxhY2hhcXVpIiwiYSI6ImNrcm50OXpvbzEwMTAyb25veWN2MG5kM3AifQ.mzkVMgGbIu7ApGVaWtPAbg';
-        // var map = new mapboxgl.Map({
-        // container: 'map',
-        // style: 'mapbox://styles/mapbox/streets-v11',
-        // center: [longitude, latitude],
-        // zoom: 10
-        // });
 
         this.coordinates = new HTMLelement('div', this.container, 'coordinates');
         this.coordinates.latititude = new HTMLelement('p', this.coordinates);
@@ -289,18 +344,13 @@ class Map {
         center: [longitude, latitude],
         zoom: 10,
         });
-
-        // map.setLayoutProperty('country-label', 'text-field', [
-        //     'get',
-        //     'name_' + `${this.language}`
-        // ]);
     }
 
     displayCoordinates(latitude, longitude) {
         const timeSwitch = (coordinate) => {
             const minutes = coordinate % 1 * 60;
             const seconds = minutes % 1 * 60;
-            return `${Math.floor(coordinate)}\xB0 ${Math.floor(minutes)}" ${Math.floor(seconds)}'`
+            return `${Math.round(coordinate)}\xB0 ${Math.abs(Math.floor(minutes))}" ${Math.abs(Math.floor(seconds))}'`/* : `${Math.round(coordinate)}\xB0 ${-Math.floor(minutes)}" ${-Math.floor(seconds)}'`*/
         }
         this.coordinates.latititude.innerHTML = this.language === 'en' ? `Latitude: ${timeSwitch(latitude)}` : `Szerokość geograficzna: ${timeSwitch(latitude)}`;
         this.coordinates.longitude.innerHTML = this.language === 'en' ? `Longitude: ${timeSwitch(longitude)}` : `Długość geograficzna: ${timeSwitch(longitude)}`;
@@ -323,20 +373,38 @@ class HTMLelement {
         return element;
     }
 }
-const omena = new WeatherApp();
 
-function getBackground() {
-    fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=bd26977b2f064d0d84133023c5cac316&tags=sky,weather,${omena.weather.description.innerHTML}&tag_mode=all&extras=url_h,url_l&format=json&nojsoncallback=1`)
-    .then(response => response.json())
-    .then(object => {
-        console.log(object);
-        const randomIndex = Math.floor(Math.random() * 100);
-        const randomPhoto = object.photos.photo[randomIndex];
-        const url = randomPhoto.url_h ? randomPhoto.url_h : randomPhoto.url_l
-        // if (!url) { url = `${}` }
-        // const url = `https://live.staticflickr.com/${randomPhoto.server}/${randomPhoto.id}_${randomPhoto.secret}_b.jpg`
-        document.body.setAttribute('style', `background-image: url(${url});`);        
-    })
+class Speech {
+    constructor() {
+        const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+        this.recognition = new SpeechRecognition();
+        this.recognition.interimResults = true;
+    }
+
+    voiceSearchListener() {
+        const voiceType = document.querySelector('.voiceButton');
+
+        voiceType.addEventListener('click', () => {
+            this.recognition.start();
+            voiceType.classList.toggle('active-button');
+
+            this.recognition.addEventListener('result', (event) => {
+                // console.log(event.results[0][0].transcript);
+                if (event.results[0].isFinal) {
+                    const searchBar = document.querySelector('.searchBar');
+                    searchBar.value = `${event.results[0][0].transcript}`;
+                    omena.getLocationBasedWeather(searchBar.value);
+                    // this.recognition.abort();
+                    voiceType.classList.remove('active-button');
+                }
+            });
+            this.recognition.addEventListener('end', () => {
+                this.recognition.abort();
+                voiceType.classList.remove('active-button');
+
+            })
+        })
+    }
 }
 
-// getBackground();
+const omena = new WeatherApp();
